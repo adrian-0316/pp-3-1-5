8454b7ef9c75a05733ed3921878714cbdb098f10
adrian-0316 <112898743+adrian-0316@users.noreply.github.com>
2023-05-10 18:02:45 +0300

    Add files via upload

---
+++ pp-3-1-5/src/main/java/ru/kata/spring/boot_security/demo/controller/RestUserController.java
@@ -0,0 +1,72 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin/users")
+public class RestUserController {
+
+    private final UserService userService;
+    //private final PasswordEncoder passwordEncoder;
+
+    public RestUserController(UserService userService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        //this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.listAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        User user = userService.getById(id);
+        return ResponseEntity.ok().body(user);
+        //return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<User> addNewUser(@RequestBody User user) {
+        //user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userService.save(user);
+        return ResponseEntity.ok().body(user);
+        //return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PutMapping("")
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        //user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userService.update(user);
+        return ResponseEntity.ok().body(user);
+        //return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        userService.deleteById(id);
+        return new ResponseEntity<>("User with ID = " + id + " was deleted", HttpStatus.OK);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<User> getAuthenticatedUser(Principal principal) {
+        User user = userService.getUserByEmail(principal.getName());
+        return ResponseEntity.ok().body(user);
+        //return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+}
